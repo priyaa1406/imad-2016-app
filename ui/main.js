fb4e2fc8669b629ddebd4351c32e04b9fcc3b260
priyaa1406 <dharshini2050@gmail.com>
2016-09-30 00:58:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onClick = function () {
    request.open("GET", "http://http://priyaa1406.imad.hasura-app.io/counter", true);
    request.send(null);
 };
+
+// Submit name 
+var nameInput = document.getElementById('name');
+var name = nameInput.vale;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function ()
+{
+    // Make a request to the server and send the name 
+    // Capture a list of names and render it as a list
+    var names = ['name', 'name2', 'name3', 'name4'];
+    var list = "";
+    for (var i=0; i< names.length; i++) {
+        list == '<li>' + name[i] + '<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
