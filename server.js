b797329b881430699b0f74684d50aab008d45515
priyaa1406 <dharshini2050@gmail.com>
2016-09-29 22:57:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,10 +115,9 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 
-app.get('/counter', funtion (req, res) 
-{
-    counter = counter +1;
-    res.send(counter,toString());
+app.get('/counter', function (req, res) {
+  counter = counter +1;
+  res.send(counter,toString());
 });
 
 app.get('/:articleName', function(req, res) {
