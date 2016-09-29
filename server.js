9afe5cd0459061b54983cbc4eeb0e46238a6dce7
priyaa1406 <dharshini2050@gmail.com>
2016-09-29 22:55:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req, res) {
+var counter = 0;
+
+app.get('/counter', funtion (req, res) 
+{
+    counter = counter +1;
+    res.send(counter,toString());
+});
+
+app.get('/:articleName', function(req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
